18b1d6f79db68af08f947bd1f94f54b5bc5a209a
JoseLu-Dev <joseludev@gmail.com>
2021-04-23 21:33:30 +0200

    Deketed useless code

--- src/app/auth/common/auth.guard.ts
+++ src/app/auth/common/auth.guard.ts
@@ -2,7 +2,7 @@ import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { take, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { AlertController } from '@ionic/angular';
 
 @Injectable({
@@ -13,15 +13,14 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     return this.auth.user.pipe(
-      take(1),
       map(user => {
+        console.log(`Logged user ${JSON.stringify(user)}`);
         if (!user) {
           this.alertCtrl.create({
             header: 'Unauthorized',
             message: 'You are not allowed to access that page.',
             buttons: ['OK']
           }).then(alert => alert.present());
-
           this.router.navigateByUrl('/');
           return false;
         } else {
