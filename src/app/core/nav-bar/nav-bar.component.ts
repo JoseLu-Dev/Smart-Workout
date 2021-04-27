1546a9b6d9b0d985d5566d8a0edd9d61654177a2
JoseLu-Dev <joseludev@gmail.com>
2021-04-27 20:36:16 +0200

    Added top navbar

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -8,19 +8,44 @@ import { Router } from '@angular/router';
 })
 export class NavBarComponent implements OnInit {
 
+  inCalendar = false;
+  inDay = true;
+  inProfile = false;
+
+  inSettings = false;
+
   constructor(private router: Router,) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  goToProfile(){
+  goToProfile() {
     this.router.navigate(['app/profile'], { replaceUrl: true });
+    this.resetInVariablesStatus();
+    this.inProfile = true;
   }
 
-  goToDay(){
+  goToDay() {
     this.router.navigate(['app/day'], { replaceUrl: true });
+    this.resetInVariablesStatus();
+    this.inDay = true;
   }
 
-  goToCalendar(){
+  goToCalendar() {
     this.router.navigate(['app/calendar'], { replaceUrl: true });
+    this.resetInVariablesStatus();
+    this.inCalendar = true;
+  }
+
+  goToSettings() {
+    this.router.navigate(['app/settings'], { replaceUrl: true });
+    this.resetInVariablesStatus();
+    this.inSettings = true;
+  }
+
+  resetInVariablesStatus() {
+    this.inCalendar = false;
+    this.inDay = false;
+    this.inProfile = false;
+    this.inSettings = false;
   }
 }
