25ff2c3998cffef6bcd65d5dd4e4d7867f0df878
JoseLu-Dev <joseludev@gmail.com>
2021-05-05 15:06:12 +0000

    Added training page

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -27,9 +27,14 @@ const routes: Routes = [
       {
         path: 'settings',
         loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
-      }
+      },
+      {
+        path: 'trainings',
+        loadChildren: () => import('./training/training.module').then( m => m.TrainingPageModule)
+      },
     ]
   },
+  
 ];
 
 @NgModule({
