197ab3abacc3b850dcd40afc501a5fd172b25ba4
JoseLu-Dev <joseludev@gmail.com>
2021-05-06 07:28:03 +0000

    Removed unused imports

--- src/app/core/common/trainings-list-widget/trainings-list-widget.module.ts
+++ src/app/core/common/trainings-list-widget/trainings-list-widget.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
 import { TrainingsListComponent } from './trainings-list/trainings-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    IonicModule,
   ],
   declarations: [TrainingsListComponent,],
   exports: [TrainingsListComponent,]
