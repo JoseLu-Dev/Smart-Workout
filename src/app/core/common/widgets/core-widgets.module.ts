5811ed09a2dd73db096167b3c8b6845636cf19ff
JoseLu-Dev <joseludev@gmail.com>
2021-05-08 22:06:45 +0200

    Missing imports

--- src/app/core/common/widgets/core-widgets.module.ts
+++ src/app/core/common/widgets/core-widgets.module.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ExercisesSetListComponent } from './exercises-set-list/exercises-set-list.component';
 import { TrainingsListComponent } from './trainings-list/trainings-list.component';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [TrainingsListComponent, ExercisesSetListComponent],
   exports: [TrainingsListComponent, ExercisesSetListComponent]
 })
