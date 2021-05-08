68a410be789905c9d4d3454f5c006e08e9fec61d
JoseLu-Dev <joseludev@gmail.com>
2021-05-08 19:15:41 +0200

    Removed unused imports

--- src/app/core/common/widgets/core-widgets.module.ts
+++ src/app/core/common/widgets/core-widgets.module.ts
@@ -1,13 +1,9 @@
 import { ExercisesSetListComponent } from './exercises-set-list/exercises-set-list.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { TrainingsListComponent } from './trainings-list/trainings-list.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
   declarations: [TrainingsListComponent, ExercisesSetListComponent],
   exports: [TrainingsListComponent, ExercisesSetListComponent]
 })
