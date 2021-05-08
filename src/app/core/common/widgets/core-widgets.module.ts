c42224347780ef3391057a028075dc0f2e4bb003
JoseLu-Dev <joseludev@gmail.com>
2021-05-08 19:17:17 +0200

    Reordered imports

--- src/app/core/common/widgets/core-widgets.module.ts
+++ src/app/core/common/widgets/core-widgets.module.ts
@@ -1,6 +1,6 @@
-import { ExercisesSetListComponent } from './exercises-set-list/exercises-set-list.component';
 import { NgModule } from '@angular/core';
 
+import { ExercisesSetListComponent } from './exercises-set-list/exercises-set-list.component';
 import { TrainingsListComponent } from './trainings-list/trainings-list.component';
 
 @NgModule({
