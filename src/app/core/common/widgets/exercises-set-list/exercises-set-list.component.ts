846347144306062e91b6d5c8cee5d5a4f16eb704
JoseLu-Dev <joseludev@gmail.com>
2021-05-10 08:29:37 +0000

    Basic exercises set list logic added

--- src/app/core/common/widgets/exercises-set-list/exercises-set-list.component.ts
+++ src/app/core/common/widgets/exercises-set-list/exercises-set-list.component.ts
@@ -7,10 +7,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExercisesSetListComponent implements OnInit {
 
-  public exercisesSets = new Array(5);
+  public exercisesSets = [
+    {
+      set: [
+        {
+          exercise: {
+            name: 'front lever', progression: 'full', variation: 'prone',
+            bodyWeight: true, muscle: 'lats', static: true
+          },
+          quantity: 20,
+          rest: 90
+        }
+      ],
+      setsCount: 4,
+      finalRest: 180
+    },
+    {
+      set: [
+        {
+          exercise: {
+            name: 'front lever pull ups', progression: 'full', variation: 'prone',
+            bodyWeight: true, muscle: 'lats', static: false
+          },
+          intensity: {
+            band: { color: '#dfed1c', fullUse: true, weight: -7 },
+            weight: -10
+          },
+          quantity: 5,
+          rest: 90
+        }
+      ],
+      setsCount: 4,
+      finalRest: 180
+    },
+    {
+      set: [
+        {
+          exercise: {
+            name: 'pull ups', variation: 'prone',
+            bodyWeight: true, muscle: 'lats', static: false
+          },
+          quantity: 10,
+        }
+      ],
+      type: 'emom',
+      setsCount: 4,
+      finalRest: 180
+    },
+    {
+      set: [
+        {
+          exercise: {
+            name: 'pull ups', variation: 'prone',
+            bodyWeight: true, muscle: 'lats', static: false
+          },
+          quantity: 8,
+        },
+        {
+          exercise: {
+            name: 'pull ups', variation: 'supine',
+            bodyWeight: true, muscle: 'lats', static: false
+          },
+          quantity: 12,
+          rest: 120
+        }
+      ],
+      type: 'super set',
+      setsCount: 4,
+      finalRest: 180
+    }
+  ];
+
+  public UserBodyWeight = 60;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
