27ff694cabd30570f2535324dcd9ae17b93ada54
JoseLu-Dev <joseludev@gmail.com>
2021-05-16 22:56:29 +0200

    Exercise model updated properties names

--- src/app/core/common/models/exercise.model.ts
+++ src/app/core/common/models/exercise.model.ts
@@ -1,5 +1,14 @@
 export class Exercise{
     name: string;
+    variation: string;
+    progression: string;
+    muscleGroup: string;
+    bodyWeight: boolean;
+    static: boolean;
+}
+
+export class ExerciseSpecs{
+    name: string;
     variations: string[];
     progressions: string[];
     muscleGroup: string;
