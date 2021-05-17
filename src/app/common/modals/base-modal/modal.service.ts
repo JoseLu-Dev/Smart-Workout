e6639a0f5c9d5c594e7b4469dbd1efac1466bffa
JoseLu-Dev <joseludev@gmail.com>
2021-05-17 21:55:44 +0200

    Base modal module created

---
+++ src/app/common/modals/base-modal/modal.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ModalService {
+    private modals: any[] = [];
+
+    /**
+     * add modal to array of active modals
+     *
+     * @param id modal id
+     */
+    add(modal: any) {
+        this.modals.push(modal);
+    }
+
+    /**
+     * remove modal from array of active modals
+     *
+     * @param id modal id
+     */
+    remove(id: string) {
+
+        this.modals = this.modals.filter(x => x.id !== id);
+    }
+
+    /**
+     * open modal specified by id
+     *
+     * @param id modal id
+     */
+    open(id: string) {
+        const modal = this.modals.find(x => x.id === id);
+        modal.open();
+    }
+
+    /**
+     * close modal specified by id
+     *
+     * @param id modal id
+     */
+    close(id: string) {
+        const modal = this.modals.find(x => x.id === id);
+        modal.close();
+    }
+}
