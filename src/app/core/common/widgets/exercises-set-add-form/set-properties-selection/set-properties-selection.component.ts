357eed9a2b065bac370b2d02a36835733f26d71f
JoseLu-Dev <joseludev@gmail.com>
2021-05-20 08:54:22 +0000

    Set properties component finished

--- src/app/core/common/widgets/exercises-set-add-form/set-properties-selection/set-properties-selection.component.ts
+++ src/app/core/common/widgets/exercises-set-add-form/set-properties-selection/set-properties-selection.component.ts
@@ -1,6 +1,6 @@
 import { Exercise } from './../../../models/exercise.model';
-import { BandUsed } from './../../../models/exercise-set.model';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { BandUsed, ExerciseSetPart, Intensity } from './../../../models/exercise-set.model';
+import { Component, Input, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BandsSelectionModalComponent } from '../../../modals/bands/bands-selection-modal/bands-selection-modal.component';
 
@@ -11,15 +11,35 @@ import { BandsSelectionModalComponent } from '../../../modals/bands/bands-select
 })
 export class SetPropertiesSelectionComponent implements OnInit {
 
+  /**
+   * 
+   */
   @Input() public exercise: Exercise;
 
+  /**
+   * Boolean to 
+   */
   @Input() public restBetweenExercises: boolean;
 
+  /**
+   * 
+   */
+  @Output() public setPart = new EventEmitter<ExerciseSetPart>();;
+
+  /**
+   * Instance of the child component (BandsModal) to use its methods
+   */
   @ViewChild(BandsSelectionModalComponent)
   bandsModal: BandsSelectionModalComponent;
 
+  /**
+   * Band selected on band modal
+   */
   public bandUsed: BandUsed;
 
+  /**
+   * Component's form
+   */
   public setFormGroup: FormGroup;
 
   constructor(private formBuilder: FormBuilder,) { }
@@ -28,22 +48,67 @@ export class SetPropertiesSelectionComponent implements OnInit {
     this.buildForm();
   }
 
+  /**
+   * Builds the component's form
+   */
   buildForm(): void {
     this.setFormGroup = this.formBuilder.group({
       weight: ['', Validators.compose([!this.exercise.bodyWeight ? Validators.required : null, Validators.min(0)])],
-      bandWeight: [{value: '', disabled: true}],
+      weightResistanceType: ['ballast'],
+      bandWeight: [{ value: '', disabled: true }],
+      bandResistanceType: ['assistance'],
       reps: ['', Validators.required],
-      restSeconds: ['30', Validators.compose([Validators.required, Validators.min(0)])],
-      restMinutes: ['1', Validators.compose([Validators.required, Validators.min(0)])],
+      restSeconds: this.restBetweenExercises ? [30, Validators.compose([Validators.required, Validators.min(0)])] : null,
+      restMinutes: this.restBetweenExercises ? [1, Validators.compose([Validators.required, Validators.min(0)])] : null,
     });
+    //TODO: disable assistance weight when exercise is not bodyweight
   }
 
-  onAddBandClicked(){
+  /**
+   * Opens band modal
+   */
+  onAddBandClicked() {
     this.bandsModal.openModal();
   }
 
-  getBandUsedFromModal(band: BandUsed){
+  /**
+   * Gets the band selected in band modal
+   * 
+   * @param band band outputed from the band modal
+   */
+  getBandUsedFromModal(band: BandUsed) {
     this.bandUsed = band;
     this.setFormGroup.get('bandWeight').setValue(this.bandUsed.weight);
+  }
+
+  /**
+   * Builds the ExerciseSetPart object and outputs it to the parent
+   * 
+   * @param form form data
+   */
+  onSubmit(form: {
+    weight: number, weightResistanceType: string,
+    bandWeight: number, bandResistanceType: string,
+    reps: number, restSeconds: number, restMinutes: number
+  }) {
+    const setPart: ExerciseSetPart = new ExerciseSetPart();
+    setPart.exercise = this.exercise;
+    setPart.quantity = form.reps;
+
+    if (form?.restMinutes && form?.restSeconds) {
+      setPart.rest = form.restMinutes * 60 + form.restSeconds;
+    }
+
+    setPart.intensity = new Intensity();
+
+    if (this.bandUsed) {
+      this.bandUsed.weight *= form.bandResistanceType == 'assistance' ? -1 : 1;
+      setPart.intensity.band = this.bandUsed;
+    }
+
+    form.weight *= form.weightResistanceType == 'assistance' ? -1 : 1;
+    setPart.intensity.weight = form.weight;
+
+    this.setPart.emit(setPart);
   }
 }
