8e0e6beeebf5a1e98937b12b2eb65d11f02d1d8e
JoseLu-Dev <joseludev@gmail.com>
2021-05-25 21:49:48 +0200

    Set properties form now can receive data to edit

--- src/app/core/common/widgets/exercises-set-add-form/set-properties-selection/set-properties-selection.component.ts
+++ src/app/core/common/widgets/exercises-set-add-form/set-properties-selection/set-properties-selection.component.ts
@@ -1,6 +1,7 @@
+import { UserStatsService } from './../../../services/user-stats.service';
 import { Exercise } from './../../../models/exercise.model';
 import { BandUsed, ExerciseSetPart, Intensity } from './../../../models/exercise-set.model';
-import { Component, Input, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, ViewChild, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BandsSelectionModalComponent } from '../../../modals/bands/bands-selection-modal/bands-selection-modal.component';
 
@@ -17,14 +18,26 @@ export class SetPropertiesSelectionComponent implements OnInit {
   @Input() public exercise: Exercise;
 
   /**
-   * Boolean to
+   * Boolean to indicate if there is rest between each exercise
    */
   @Input() public restBetweenExercises: boolean;
 
   /**
-   * Boolean to
+   * Set Part to be edited in the form
    */
-  @Input() public setPartToEdit: ExerciseSetPart;
+  @Input() public set setPartToEdit(setPart: ExerciseSetPart) {
+    if (!setPart) { return; }
+    console.log(setPart);
+    this.exercise = setPart.exercise;
+
+    if (setPart?.intensity?.band) {
+      this.bandUsed = setPart.intensity.band;
+    }
+
+    this.buildForm(setPart);
+
+    this.changeDetector.detectChanges();
+  };
 
   /**
    * Set part outputed when selected
@@ -47,26 +60,43 @@ export class SetPropertiesSelectionComponent implements OnInit {
    */
   public setFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userStats: UserStatsService,
+    private changeDetector: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
-    this.buildForm();
+    if (!this.setFormGroup) {
+      this.buildForm(null);
+    }
   }
 
   /**
    * Builds the component's form
    */
-  buildForm(): void {
+  buildForm(setPart: ExerciseSetPart): void {
+    const values: FormValues = this.getFormValues(setPart);
+
     this.setFormGroup = this.formBuilder.group({
-      weight: ['', Validators.compose([!this.exercise.bodyWeight ? Validators.required : null, Validators.min(0)])],
-      weightResistanceType: ['ballast'],
-      bandWeight: [{ value: '', disabled: true }],
-      bandResistanceType: ['assistance'],
-      reps: ['', Validators.required],
+      weight: [values.weight, Validators.compose([!this.exercise.bodyWeight ? Validators.required : null, Validators.min(1)])],
+      weightResistanceType: [{ value: values.weightResistanceType, disabled: this.exercise.bodyWeight ? false : true }],
+      bandWeight: [{ value: values.bandWeight, disabled: true }],
+      bandResistanceType: [values.bandResistanceType],
+      reps: [values.reps, Validators.compose([Validators.required, Validators.min(1)])],
       restSeconds: this.restBetweenExercises ? [30, Validators.compose([Validators.required, Validators.min(0)])] : null,
       restMinutes: this.restBetweenExercises ? [1, Validators.compose([Validators.required, Validators.min(0)])] : null,
     });
-    //TODO: disable assistance weight when exercise is not bodyweight
+  }
+
+  /**
+   * Gets the form initial values
+   *
+   * @param setPart object used to initialize form values
+   * @returns form values
+   */
+  getFormValues(setPart: ExerciseSetPart): FormValues {
+    return new FormValues(setPart, this.userStats.getWeight());
   }
 
   /**
@@ -116,8 +146,80 @@ export class SetPropertiesSelectionComponent implements OnInit {
     }
 
     form.weight *= form.weightResistanceType === 'assistance' ? -1 : 1;
+    form.weight += this.exercise.bodyWeight ? this.userStats.getWeight() : 0;
     setPart.intensity.weight = form.weight;
 
     this.setPart.emit(setPart);
+  }
+}
+
+class FormValues {
+  weight: string;
+  weightResistanceType: string;
+  bandWeight: number;
+  bandResistanceType: string;
+  reps: string;
+  restSeconds: number;
+  restMinutes: number;
+
+  constructor(setPart: ExerciseSetPart, userWeight: number) {
+    this.setDefaultValues();
+
+    if (setPart) {
+      this.setSetPartValues(setPart, userWeight);
+    }
+  }
+
+  setSetPartValues(setPart: ExerciseSetPart, userWeight: number) {
+    if (setPart?.intensity?.weight) {
+      let weight = setPart.intensity.weight;
+      if (setPart.exercise.bodyWeight) {
+        weight -= userWeight;
+      }
+      this.weight = Math.abs(weight).toString();
+      this.weightResistanceType = weight < 0 ? 'assistance' : 'ballast';
+    }
+
+    if (setPart?.intensity?.band) {
+      this.bandWeight = Math.abs(setPart.intensity.band.weight);
+      this.bandResistanceType = setPart.intensity.band.weight < 0 ? 'assistance' : 'ballast';
+    }
+
+    if (setPart?.quantity) {
+      this.reps = setPart.quantity.toString();
+    }
+
+    if (setPart?.rest) {
+      const restSeconds = setPart.rest;
+      this.restMinutes = Math.trunc(restSeconds / 60);
+      this.restSeconds = restSeconds % 60;
+    }
+  }
+
+  /**
+   * Default form values
+   */
+  setDefaultValues() {
+    this.setDefaultWeightValues();
+    this.setDefaultRepsValue();
+    this.setDefaultRestValues();
+  }
+
+  setDefaultWeightValues() {
+    this.weightResistanceType = 'ballast';
+    this.weight = '';
+
+    this.bandWeight = 0;
+    this.bandResistanceType = 'assistance';
+  }
+
+  setDefaultRepsValue() {
+    this.reps = '';
+  }
+
+  setDefaultRestValues() {
+    //TODO: custom default rest
+    this.restSeconds = 30;
+    this.restMinutes = 1;
   }
 }
