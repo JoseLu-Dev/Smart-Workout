3ebb366a1d4e204511e6432cd26ed5f9cb1509df
JoseLu-Dev <joseludev@gmail.com>
2021-06-02 20:04:43 +0200

    Chart mocked

--- src/app/core/profile/trainings-graphs/trainings-graphs.component.ts
+++ src/app/core/profile/trainings-graphs/trainings-graphs.component.ts
@@ -19,14 +19,15 @@ export class TrainingsGraphsComponent implements OnInit {
   ngOnInit() {
 
     //Sort trainings stats by date
-    this.trainingStats.sort((a, b) => a.date > b.date ? 1 : 0)
+    this.trainingStats.sort((a, b) => a.date > b.date ? 1 : 0);
 
     this.addEmptyDays();
 
     const musclesArray = this.getMusclesTrained();
 
 
-    console.log(this.trainingStats)
+
+    console.log(this.trainingStats);
 
     this.setChartLabels();
     this.setChartData(musclesArray);
@@ -37,52 +38,71 @@ export class TrainingsGraphsComponent implements OnInit {
       const date = new Date(tr.date);
       this.chartLabels.push(`${date.getDate()}/${date.getMonth()}`);
       delete tr.date;
-    })
+    });
   }
 
   setChartData(musclesArray: string[]) {
+    this.data.push({ label: 'total', data: [] });
     musclesArray.forEach(element => {
-      this.data.push({ label: element, data: [] })
+      this.data.push({ label: element, data: [] });
+    });
+
+    this.trainingStats.forEach((trStat) => {
+      trStat['setsNumber'] = new Object();
+      musclesArray.forEach((muscle) => {
+        trStat['setsNumber'][muscle] = 0;
+      });
     });
 
     this.trainingStats.forEach((trSt) => {
       let total = 0;
-      trSt['setsNumber'] = new Object();
-      trSt.sets.map((set)=>{
+      trSt.sets.map((set) => {
         total += set.number;
-        trSt['setsNumber'][set.muscleGroup]=set.number;
-      })
+        trSt['setsNumber'][set.muscleGroup] = set.number;
+      });
       trSt['setsNumber']['Total'] = total;
-      
-      delete trSt.sets
-    })
-    console.log(this.trainingStats)
+
+      delete trSt.sets;
+    });
+
+    this.data.reverse();
+
+    this.trainingStats.forEach((trStats, index) => {
+      let i = 0;
+      // eslint-disable-next-line guard-for-in
+      for (const muscle in trStats['setsNumber']){
+
+        this.data[i]['data'].push(trStats['setsNumber'][muscle]);
+        i++;
+      }
+    });
+
+    this.data.reverse();
   }
 
   getMusclesTrained() {
     //Muscles trained during this time
-    let muscles = new Object();
-    muscles['total'] = null
+    const muscles = new Object();
     this.trainingStats.forEach((trStats) => {
       trStats.sets.forEach((set) => {
         muscles[set.muscleGroup] = null;
-      })
-    })
+      });
+    });
     return Object.keys(muscles);
   }
 
   addEmptyDays() {
     for (let i = 0; i < this.trainingStats.length; i++) {
 
-      let date = new Date(this.trainingStats[i].date);
-      date.setDate(date.getDate() + 1)
+      const date = new Date(this.trainingStats[i].date);
+      date.setDate(date.getDate() + 1);
 
       if (i + 1 >= this.trainingStats.length) { break; }
       while (date < new Date(this.trainingStats[i + 1].date)) {
         const trainingStats = new TrainingStats();
         trainingStats.date = JSON.parse(JSON.stringify(date));
-        date.setDate(date.getDate() + 1)
-        this.trainingStats.splice(i + 1, 0, trainingStats)
+        date.setDate(date.getDate() + 1);
+        this.trainingStats.splice(i + 1, 0, trainingStats);
       }
     }
   }
@@ -127,5 +147,5 @@ export class TrainingsGraphsComponent implements OnInit {
         }
       ]
     }
-  ]
+  ];
 }
