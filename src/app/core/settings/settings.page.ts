067d23c6bf9c59db0fd9b934539294a0928c36c1
JoseLu-Dev <joseludev@gmail.com>
2021-06-20 16:33:04 +0200

    Added logout button

--- src/app/core/settings/settings.page.ts
+++ src/app/core/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/common/auth.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogOutButtonCLicked() {
+    this.authService.logout();
   }
 
 }
