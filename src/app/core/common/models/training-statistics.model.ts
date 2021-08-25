63aa165f57ae9b8b85b4a08f641c10d5681de2b7
JoseLu-Dev <joseludev@gmail.com>
2021-08-25 19:45:10 +0200

    Added resps per rest time

--- src/app/core/common/models/training-statistics.model.ts
+++ src/app/core/common/models/training-statistics.model.ts
@@ -7,6 +7,10 @@ export class TrainingStatistics {
     setsPerMuscle: CountPerMuscle;
     repsPerMuscle: CountPerMuscle;
     weightPerMuscle: CountPerMuscle;
+
+    repsPerRestMinute(): number {
+        return Math.round(this.totalReps / (this.totalRest / 60) * 1000) / 1000;
+    }
 }
 
 export class CountPerMuscle {
